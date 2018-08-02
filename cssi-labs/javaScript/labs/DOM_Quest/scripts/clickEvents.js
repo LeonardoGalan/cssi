88b737943984b4259c905b8c964f127e652ae7df
LeonardoGalan <leonardogalan2@gmail.com>
2018-08-02 15:33:52 -0400

    DOM Quesst Lab

--- cssi-labs/javaScript/labs/DOM_Quest/scripts/clickEvents.js
+++ cssi-labs/javaScript/labs/DOM_Quest/scripts/clickEvents.js
@@ -12,4 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 console.log("Running Click Events Script");
+document.getElementById('box1').addEventListener('click',()=>{boxcolor(0);});
+document.getElementById('box2').addEventListener('click',()=>{boxcolor(1);});
+document.getElementById('box3').addEventListener('click',()=>{boxcolor(2);});
+//^ This is saying in the event of clicking box 1, box 2, 3,
+
+
+const colors = ['red','pink','orange'];
+
+function boxcolor(value){ // the value specified in lines 17-20
+  let newcolor = document.getElementsByClassName('box');
+  newcolor[0].style.backgroundColor = colors[value];
+  newcolor[1].style.backgroundColor = colors[value];
+  newcolor[2].style.backgroundColor = colors[value];
+}
+// document.getElementById('box4').addEventListener('click',()=>boxcol('box4');});
+// document.getElementById('box5').addEventListener('click',()=>boxcol('box4');});
+
+function boxcol(value){
+  let cg = document.getElementById(value).classList.toggle('active');
+
+
+}
